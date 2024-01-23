549fca9de7984010974188fbc92b8b88be032414
arche <zxk44944@qq.com>
2024-01-23 17:54:16 +0800

    test route handler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [env, setEnv] = useState({});
   useEffect(() => {
-    fetch('/api/one');
-  });
-  return <h1>hello next on vercel</h1>;
+    fetch('/api/one')
+      .then((res) => res.json())
+      .then((data) => {
+        setEnv(data);
+      });
+  }, []);
+  return (
+    <>
+      <h1>hello next on vercel</h1>
+      <pre>{JSON.stringify(env, null, 2)}</pre>
+    </>
+  );
 }
